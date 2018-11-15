c29587145f2237009f5b639e17337dff9f9186c3
Yaron Malin <yaron1m@gmail.com>
2018-11-15 14:06:23 -0800

    Screens by user id

---
+++ screens/family-member/FamilyMemberScreenContainer.js
@@ -0,0 +1,14 @@
+import {connect} from 'react-redux';
+import FamilyMemberScreen from './FamilyMemberScreen';
+
+function mapStateToProps(state){
+    return {
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FamilyMemberScreen);
---
+++ screens/guardian-angel/LostPeopleAroundYouScreenContainer.js
@@ -0,0 +1,14 @@
+import {connect} from 'react-redux';
+import LostPeolpeAroundYouScreen from './LostPeopleAroundYouScreen';
+
+function mapStateToProps(state){
+    return {
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LostPeolpeAroundYouScreen);
