930e9aeb14bc4f6d00880227c13d2cef05838a24
dror <dror@sears.co.il>
2018-11-16 00:35:59 -0800

    Fix about tab

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import {ScrollView, StyleSheet} from 'react-native';
+import {Text} from 'react-native';
 
 export default class LinksScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Links',
-  };
+    static navigationOptions = {
+        title: 'About',
+    };
 
-  render() {
-    return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-          <Text>About</Text>
-      </ScrollView>
-    );
-  }
+    render() {
+        return (
+            <Text styles={{margin: 10}}>Guardian Angel app @ Facebook Hackathon 2018</Text>
+        );
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-});
