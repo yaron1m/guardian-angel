7c1b800200e5468dd63a69aa169cc0eb4957bd61
dror <dror@sears.co.il>
2018-11-15 19:27:54 -0800

    Add MissingPerson section

--- components/ActionButton.js
+++ components/ActionButton.js
@@ -4,7 +4,7 @@ import {Button, StyleSheet, View} from 'react-native';
 export default function ActionButton(props) {
     return (
         <View style={styles.buttonContainer}>
-            <Button title={props.title} onPress={props.onPress} color={"#d66a00"} buttonStyle={styles.button}/>
+            <Button title={props.title} onPress={props.onPress} color={"#d66a00"}/>
         </View>
     );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -19,6 +19,5 @@ export default class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
   },
 });
