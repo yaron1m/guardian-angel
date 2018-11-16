1b73a17435da5109a6d91ffbf3d9f567658c1d10
Doron <doron@sears.co.il>
2018-11-16 09:04:52 +0200

    update location

--- components/map/LocationMap.js
+++ components/map/LocationMap.js
@@ -7,8 +7,8 @@ const {Marker} = MapView;
 class LocationMap extends Component {
     render() {
         const initialRegion = {
-            latitude: this.props.lat,
-            longitude: this.props.long,
+            latitude: this.props.center.lat,
+            longitude: this.props.center.long,
             latitudeDelta: 0.0017,
             longitudeDelta: 0.0015,
         };
@@ -25,6 +25,7 @@ class LocationMap extends Component {
         return (
             <View style={{flexGrow: 1, alignItems: 'center', justifyContent: 'center'}}>
                 <MapView style={{alignSelf: 'stretch', height: 300}} initialRegion={initialRegion}>
+                    {getMarker(this.props.center)}
                     {this.props.points.map(point => (
                         getMarker(point)
                         ))}
--- screens/family-member/FamilyMemberMap.js
+++ screens/family-member/FamilyMemberMap.js
@@ -3,32 +3,8 @@ import LocationMap from '../../components/map/LocationMap';
 
 export default function FamilyMemberMap({user}) {
     const knownLocations = Object.keys(user.knownLocations).map((key) => user.knownLocations[key]);
-    getCenter();
 
-    function getCenter() {
-        let allLocations = Object.assign(knownLocations);
-        allLocations.push(user.location);
-
-        let latSum = 0;
-        for (let i = 0; i < allLocations.length; i++) {
-            latSum += allLocations[i].lat; //don't forget to add the base
-        }
-
-        const latAvg = latSum / allLocations.length;
-
-        let longSum = 0;
-        for (let i = 0; i < allLocations.length; i++) {
-            longSum += allLocations[i].long; //don't forget to add the base
-        }
-
-        const longAvg = longSum / allLocations.length;
-
-        return {
-            lat: latAvg,
-            long: longAvg
-        }
-    }
-
-    return (<LocationMap lat={getCenter().lat} long={getCenter().long} points={knownLocations}/>);
-
+    return (
+        <LocationMap center={user.location} points={knownLocations}/>
+    );
 }
