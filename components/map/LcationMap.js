6fa8c36ce4ff3f5da56270d00076537c4b98ec35
Doron <doron@sears.co.il>
2018-11-16 02:34:44 +0200

    change LocationMap

--- components/map/LcationMap.js
+++ components/map/LcationMap.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import {View} from "react-native";
 import {MapView} from "expo";
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
 
+class GeolocationExample extends Component {
+    constructor(props) {
+        super(props);
 
-export default function LocationMap() {
-    return (
-        <View>
-            <MapView style={{alignSelf: 'stretch', height: 200}}
-                     initialRegion={{
-                         latitude: 37.78825,
-                         longitude: -122.4324,
-                         latitudeDelta: 0.0922,
-                         longitudeDelta: 0.0421,
-                     }}/>
-        </View>)
+        this.state = {
+            latitude: null,
+            longitude: null,
+            error: null,
+        };
+    }
+
+    render() {
+        return (
+            <View style={{flexGrow: 1, alignItems: 'center', justifyContent: 'center'}}>
+                <Text>Latitude: {this.state.latitude}</Text>
+                <Text>Longitude: {this.state.longitude}</Text>
+                <MapView style={{alignSelf: 'stretch', height: 200}}
+                         initialRegion={{
+                             latitude: this.state.latitude,
+                             longitude: this.state.longitude,
+                             latitudeDelta: 0,
+                             longitudeDelta: 0,
+                         }}/>
+                {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+            </View>
+        );
+    }
 }
+
+export default GeolocationExample;
