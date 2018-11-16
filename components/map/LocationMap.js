1e91cda4d0dd6c7fce7cee054257f6b66c002f72
Doron <doron@sears.co.il>
2018-11-16 08:56:31 +0200

    add map

--- components/map/LocationMap.js
+++ components/map/LocationMap.js
@@ -2,33 +2,34 @@ import {MapView} from "expo";
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
+const {Marker} = MapView;
+
 class LocationMap extends Component {
     render() {
         const initialRegion = {
             latitude: this.props.lat,
             longitude: this.props.long,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
+            latitudeDelta: 0.0017,
+            longitudeDelta: 0.0015,
         };
 
-        console.log(initialRegion);
+        function getMarker(point) {
+            return (<Marker
+                key={point.name}
+                title={point.name}
+                coordinate={{longitude: point.long, latitude: point.lat}}
+                pinColor={point.pinColor}
+            />);
+        }
 
         return (
             <View style={{flexGrow: 1, alignItems: 'center', justifyContent: 'center'}}>
-                {/*<Text>Latitude: {this.props.lat}</Text>*/}
-                {/*<Text>Longitude: {this.props.long}</Text>*/}
-                {/*<MapView style={{alignSelf: 'stretch', height: 200}} initialRegion={initialRegion}>*/}
-                    {/*{this.props.points.map(point => (*/}
-                        {/*<MapView.MapMarker*/}
-                            {/*key={point.name}*/}
-                            {/*coordinate={{latitude:this.props.lat,longitude: this.props.long}}*/}
-                            {/*title={point.name}*/}
-                            {/*name={point.name}*/}
-                            {/*description={point.name}*/}
-                        {/*/>*/}
-                    {/*))}*/}
-                {/*</MapView>*/}
-                {/*{this.props.error && <Text>Error: {this.props.error}</Text>}*/}
+                <MapView style={{alignSelf: 'stretch', height: 300}} initialRegion={initialRegion}>
+                    {this.props.points.map(point => (
+                        getMarker(point)
+                        ))}
+                </MapView>
+                {this.props.error && <Text>Error: {this.props.error}</Text>}
             </View>
         );
     }
